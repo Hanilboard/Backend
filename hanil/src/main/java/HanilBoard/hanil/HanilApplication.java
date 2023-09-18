6480009f690d0a438aaf1dafb033473dfd5ad552
Dev-SSW <sgg919@naver.com>
2023-09-18 19:39:48 +0900

    Start!

---
+++ hanil/src/main/java/HanilBoard/hanil/HanilApplication.java
@@ -0,0 +1,13 @@
+package HanilBoard.hanil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanilApplication.class, args);
+	}
+
+}
